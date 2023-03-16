ce31f088bca891b710fd14ba7da7a52df5b727ff
Ray Sylverne <raysylverne@gmail.com>
2023-03-15 22:57:53 -0400

    add Securtity Group

--- Two_Tier_AWS/main.tf
+++ Two_Tier_AWS/main.tf
@@ -141,19 +141,34 @@ resource "aws_instance" "ubuntu_web_server" {
 }
 
 resource "aws_s3_bucket" "my_new_s3_bucket" {
-    bucket = "my-new-tf-test-bucket-rayjs"
-    
-    tags = {
-        Name    = "My S3 Bucket"
-        Purpose = "Intro to Resource Buckets Block"
-    }
+  bucket = "my-new-tf-test-bucket-rayjs"
+
+  tags = {
+    Name    = "My S3 Bucket"
+    Purpose = "Intro to Resource Buckets Block"
+  }
 }
 
 resource "aws_s3_bucket_acl" "my_new_bucket_acl" {
-    bucket = aws_s3_bucket.my_new_s3_bucket.id
-    acl    = "private"
+  bucket = aws_s3_bucket.my_new_s3_bucket.id
+  acl    = "private"
 }
 
-resource "" "name" {
-  
+resource "aws_security_group" "my-new-security-group" {
+  name        = "web_server_inbound"
+  description = "Allow inbound traffic on tcp/443"
+  vpc_id      = aws_vpc.demo_vpc.id
+
+  ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
 }
