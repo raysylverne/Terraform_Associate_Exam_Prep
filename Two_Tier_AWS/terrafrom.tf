54f01c36f4e54a5860f7ed1bb0b4dbecee5de714
Ray Sylverne <raysylverne@gmail.com>
2023-03-17 09:40:45 -0400

    add providers

--- Two_Tier_AWS/terrafrom.tf
+++ Two_Tier_AWS/terrafrom.tf
@@ -2,5 +2,27 @@
 #to their syntax that could break your code 
 
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
 }
