cf5ce9e04c4c6533f667e3ae8b10533721ce8057
Ray Sylverne <raysylverne@gmail.com>
2023-03-27 10:24:25 -0400

    add output size

--- Two_Tier_AWS/modules/server/output.tf
+++ Two_Tier_AWS/modules/server/output.tf
@@ -4,3 +4,8 @@ output "public_ip" {
 output "public_dns" {
   value = aws_instance.web_server_module.public_dns
 }
+
+output "size" {
+  description = "Size of server built with Server Module"
+  value       = aws_instance.web_server_module.instance_type
+}
