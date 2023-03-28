98aa85a56b92d669a98196dc561bc063c92cec71
Ray Sylverne <raysylverne@gmail.com>
2023-03-28 14:28:36 -0400

    add S3, ASG, EC2 Module

--- Two_Tier_AWS/main.tf
+++ Two_Tier_AWS/main.tf
@@ -283,3 +283,94 @@ resource "aws_security_group" "vpc-ping" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+module "web_server_module" {
+  source    = "./modules/server"
+  ami       = data.aws_ami.ubuntu.id
+  size      = "t2.micro"
+  subnet_id = aws_subnet.public_subnets["public_subnet_3"].id
+  security_groups = [
+    aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+}
+
+module "server_subnet_1" {
+  source      = "./modules/web_server"
+  ami         = data.aws_ami.ubuntu.id
+  key_name    = aws_key_pair.generated.key_name
+  user        = "ubuntu"
+  private_key = tls_private_key.generated.private_key_pem
+  subnet_id   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+  aws_security_group.vpc-web.id]
+}
+
+output "public_ip" {
+  value = module.web_server_module.public_ip
+}
+
+output "public_dns" {
+  value = module.web_server_module.public_dns
+}
+
+output "size" {
+  value = module.web_server_module.size
+}
+
+# Create Resources Using Modules from GitHub Repo
+module "autoscaling" {
+  source = "github.com/terraform-aws-modules/terraform-aws-autoscaling"
+
+  # Autoscaling group
+  name = "myasg"
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id,
+    aws_subnet.private_subnets["private_subnet_2"].id,
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size         = 0
+  max_size         = 2
+  desired_capacity = 1
+  # Launch template
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+}
+
+output "asg_group_size" {
+  value = module.autoscaling.autoscaling_group_max_size
+}
+
+output "asg_az" {
+  value = module.autoscaling.autoscaling_group_availability_zones
+}
+
+# Create S3 Bucket Using Modules from Terraform Public Registry
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.8.2"
+}
+
+output "s3_bucket_name" {
+  value = module.s3-bucket.s3_bucket_bucket_domain_name
+}
+
+/*
+# Create VPC Using Modules from Terraform Public Registry
+module "vpc" {
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "3.19.0"
+  name               = "my-vpc-terraform"
+  cidr               = "10.0.0.0/16"
+  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  tags = {
+    Name        = "VPC from Module"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+*/
