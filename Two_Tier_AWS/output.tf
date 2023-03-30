b40bb57bee0a2176f19607b2e64c8e6e001ced76
Ray Sylverne <raysylverne@gmail.com>
2023-03-30 19:16:41 -0400

    add ec2 arn output

--- Two_Tier_AWS/output.tf
+++ Two_Tier_AWS/output.tf
@@ -1,10 +1,12 @@
+/* 
 # This is a static output
 output "hello-world" {
   description = "Print a Hello World text output"
   value       = "Hello World"
 }
+*/
 
-# If we created a modul we could use this output block to export the VPC_id to be imported 
+# If we created a module we could use this output block to export the VPC_id to be imported 
 # into other modules with resource that need to be attached to the VPC
 output "vpc_id" {
   description = "Output the ID for the primary VPC"
@@ -25,4 +27,9 @@ output "vpc_information" {
 output "public_ip" {
   description = "Public IP for Web Server"
   value       = aws_instance.web_server.public_ip
+}
+
+output "ec2_instance_arn" {
+  value     = aws_instance.web_server.arn
+  sensitive = true
 }
