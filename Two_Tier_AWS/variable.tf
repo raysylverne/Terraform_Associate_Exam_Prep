6210cd3aaec3d381515a6c9dad9026d33814b1c1
Ray Sylverne <raysylverne@gmail.com>
2023-03-31 20:03:49 -0400

    functions

--- Two_Tier_AWS/variable.tf
+++ Two_Tier_AWS/variable.tf
@@ -103,3 +103,44 @@ variable "env" {
     }
   }
 }
+
+variable "num_1" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 88
+}
+variable "num_2" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 73
+}
+variable "num_3" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 52
+}
+
+variable "web_ingress" {
+  type = map(object(
+    {
+      description = string
+      port        = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }
+  ))
+  default = {
+    "80" = {
+      description = "Port 80"
+      port        = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    "443" = {
+      description = "Port 443"
+      port        = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
